5816fac01191b6666f6a59886def64c3e6b4a19b
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-02 20:23:59 +0200

    starter

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#2F95DC';
+const tintColorDark = '#FFFFFF';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#FFFFFF',
+    tint: tintColorLight,
+    tabIconDefault: '#CCCCCC',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#FCFCFC',
+    background: '#000000',
+    tint: tintColorDark,
+    tabIconDefault: '#CCCCCC',
+    tabIconSelected: tintColorDark,
+  },
+};
