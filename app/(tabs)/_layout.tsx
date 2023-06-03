eec7b2bea844d36f33e78ee16e5ac1a03dc3ceba
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-03 12:26:01 +0200

    clean

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,84 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
+
+import Colors from "../../constants/Colors";
+import React from "react";
+
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>["name"];
+    color: string;
+}) {
+    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+    const colorScheme = useColorScheme();
+
+    const tabs = [
+        {
+            name: "chart",
+            title: "Chart",
+            icon: "bar-chart",
+        },
+        {
+            name: "news",
+            title: "News",
+            icon: "newspaper-o",
+        },
+        {
+            name: "menu",
+            title: "Menu",
+            icon: "user",
+        },
+    ];
+
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            }}
+        >
+            <Tabs.Screen
+                name="watchlist"
+                options={{
+                    title: "Watchlist",
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="star" color={color} />
+                    ),
+                    headerRight: () => (
+                        <Link href="/modal" asChild>
+                            <Pressable>
+                                {({ pressed }) => (
+                                    <FontAwesome
+                                        name="info"
+                                        size={25}
+                                        color={
+                                            Colors[colorScheme ?? "light"].text
+                                        }
+                                        style={{
+                                            marginRight: 15,
+                                            opacity: pressed ? 0.5 : 1,
+                                        }}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                    ),
+                }}
+            />
+            {tabs.map((tab) => (
+                <Tabs.Screen
+                    key={tab.name}
+                    name={tab.name}
+                    options={{
+                        title: tab.title,
+                        tabBarIcon: ({ color }) => (
+                            <TabBarIcon name={tab.icon} color={color} />
+                        ),
+                    }}
+                />
+            ))}
+        </Tabs>
+    );
+}
