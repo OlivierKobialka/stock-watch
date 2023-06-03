adffe7dba2121a5352ea53ce3ff76fb6ed212f93
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-03 16:47:24 +0200

    import styling

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -11,7 +11,7 @@ import {
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Text, View } from "../../components/Themed";
 import React, { useCallback, useState } from "react";
-import Colors from "../../constants/Colors";
+import { Menu, Colors } from "../../constants";
 
 export default function ChartScreen() {
     const colorScheme = useColorScheme();
@@ -24,15 +24,15 @@ export default function ChartScreen() {
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Menu</Text>
+        <View style={Menu.container}>
+            <Text style={Menu.title}>Menu</Text>
             <ScrollView
                 showsVerticalScrollIndicator={false}
-                style={styles.scrollView}
+                style={Menu.scrollView}
             >
                 {/*  */}
-                <View style={styles.topInfo}>
-                    <View style={styles.user}>
+                <View style={Menu.topInfo}>
+                    <View style={Menu.user}>
                         <FontAwesome
                             name="user-circle-o"
                             color={Colors[colorScheme ?? "light"].text}
@@ -41,30 +41,30 @@ export default function ChartScreen() {
                                 marginHorizontal: 15,
                             }}
                         />
-                        <Text style={styles.text}>Olivier Kobiałka</Text>
+                        <Text style={Menu.text}>Olivier Kobiałka</Text>
                     </View>
-                    <View style={styles.boxContainer}>
-                        <TouchableOpacity style={styles.smallBox}>
+                    <View style={Menu.boxContainer}>
+                        <TouchableOpacity style={Menu.smallBox}>
                             <FontAwesome name="send-o" size={25} />
 
-                            <Text style={styles.text}>chats</Text>
+                            <Text style={Menu.text}>chats</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.smallBox}>
+                        <TouchableOpacity style={Menu.smallBox}>
                             <FontAwesome name="cog" size={25} />
-                            <Text style={styles.text}>settings</Text>
+                            <Text style={Menu.text}>settings</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
                 {/*  */}
                 <View>
-                    <View style={styles.viewBox}>
+                    <View style={Menu.viewBox}>
                         <FontAwesome
                             name="moon-o"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>Dark theme</Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>Dark theme</Text>
                             <Switch
                                 style={{
                                     transform: [
@@ -86,25 +86,25 @@ export default function ChartScreen() {
                             />
                         </View>
                     </View>
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="share-square-o"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>Share TradingView</Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>Share TradingView</Text>
                         </View>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="comment-o"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>Rate us</Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>Rate us</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -114,16 +114,14 @@ export default function ChartScreen() {
                         marginTop: 30,
                     }}
                 >
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="at"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>
-                                We're on social media
-                            </Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>We're on social media</Text>
                             <FontAwesome
                                 name="angle-right"
                                 color="#E5E5EA"
@@ -131,14 +129,14 @@ export default function ChartScreen() {
                             />
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="sticky-note-o"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>TV Stickers</Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>TV Stickers</Text>
                             <FontAwesome
                                 name="angle-right"
                                 color="#E5E5EA"
@@ -146,14 +144,14 @@ export default function ChartScreen() {
                             />
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="info"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>About</Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>About</Text>
                             <FontAwesome
                                 name="angle-right"
                                 color="#E5E5EA"
@@ -168,14 +166,14 @@ export default function ChartScreen() {
                         marginTop: 30,
                     }}
                 >
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="question-circle-o"
                             color={Colors[colorScheme ?? "light"].text}
                             size={25}
                         />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>Help center</Text>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>Help center</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -185,13 +183,13 @@ export default function ChartScreen() {
                         marginTop: 30,
                     }}
                 >
-                    <TouchableOpacity style={styles.viewBox}>
+                    <TouchableOpacity style={Menu.viewBox}>
                         <FontAwesome
                             name="sign-out"
                             color="#FF3B30"
                             size={25}
                         />
-                        <View style={styles.rectangle}>
+                        <View style={Menu.rectangle}>
                             <Text
                                 style={{
                                     color: "#FF3B30",
@@ -211,12 +209,12 @@ export default function ChartScreen() {
                     }}
                 >
                     <TouchableOpacity
-                        style={styles.viewBox}
+                        style={Menu.viewBox}
                         onPress={handleLinkPress}
                     >
                         <FontAwesome name="github" size={25} />
-                        <View style={styles.rectangle}>
-                            <Text style={styles.text}>
+                        <View style={Menu.rectangle}>
+                            <Text style={Menu.text}>
                                 Chek out Github repo
                             </Text>
                         </View>
@@ -226,84 +224,3 @@ export default function ChartScreen() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        padding: 20,
-    },
-    scrollView: {
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-    },
-    viewBox: {
-        display: "flex",
-        flexDirection: "row",
-        width: "100%",
-        justifyContent: "space-between",
-        alignItems: "center",
-        height: 50,
-    },
-    rectangle: {
-        display: "flex",
-        flexDirection: "row",
-        width: "87%",
-        height: 50,
-        justifyContent: "space-between",
-        alignItems: "center",
-        borderBottomColor: "#E5E5EA",
-        borderBottomWidth: 1,
-    },
-    text: {
-        fontSize: 16,
-        fontWeight: "400",
-        textTransform: "capitalize",
-    },
-    topInfo: {
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        marginBottom: 30,
-    },
-    user: {
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        backgroundColor: "#E5E5EA",
-        borderRadius: 10,
-        marginBottom: 10,
-        height: 70,
-    },
-    boxContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-    },
-    smallBox: {
-        height: 70,
-        width: "48%",
-        backgroundColor: "#E5E5EA",
-        borderRadius: 10,
-        justifyContent: "space-around",
-        alignItems: "flex-start",
-        paddingLeft: 15,
-        paddingVertical: 5,
-    },
-});
---
+++ constants/Menu.ts
@@ -0,0 +1,82 @@
+const userBgColor = "#edf2f7";
+
+export default {
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        padding: 20,
+    },
+    scrollView: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: "80%",
+    },
+    viewBox: {
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 50,
+    },
+    rectangle: {
+        display: "flex",
+        flexDirection: "row",
+        width: "87%",
+        height: 50,
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottomColor: userBgColor,
+        borderBottomWidth: 1,
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: "400",
+        textTransform: "capitalize",
+    },
+    topInfo: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        marginBottom: 30,
+    },
+    user: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: userBgColor,
+        borderRadius: 10,
+        marginBottom: 10,
+        height: 70,
+    },
+    boxContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+    },
+    smallBox: {
+        height: 70,
+        width: "48%",
+        backgroundColor: userBgColor,
+        borderRadius: 10,
+        justifyContent: "space-around",
+        alignItems: "flex-start",
+        paddingLeft: 15,
+        paddingVertical: 5,
+    },
+};
