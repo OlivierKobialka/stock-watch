bba8a6fa1c06e6b44b2d08c9c3a37d3f6e96e448
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-03 12:55:31 +0200

    menu struckture

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,13 +1,74 @@
-import { StyleSheet } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Switch } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import React from "react";
 
 export default function ChartScreen() {
+    const [SwithcValue, setSwitchValue] = React.useState(false);
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Menu</Text>
+            <ScrollView>
+                <View>
+                    <View>
+                        <Text>username</Text>
+                    </View>
+                    <View>
+                        <View>
+                            <Text>chats</Text>
+                        </View>
+                        <View>
+                            <Text>settings</Text>
+                        </View>
+                    </View>
+                </View>
+                {/*  */}
+                <View>
+                    <View>
+                        <Text>Dark theme</Text>
+                        <Switch
+                            value={SwithcValue}
+                            onValueChange={() => setSwitchValue(!SwithcValue)}
+                            ios_backgroundColor={"#F2F2F7"}
+                            trackColor={{ false: "#F2F2F7", true: "#007AFF" }}
+                            thumbColor={"#f4f3f4"}
+                            disabled={false}
+                        />
+                    </View>
+                    <Pressable>
+                        <Text>Share TradingView</Text>
+                    </Pressable>
+                    <Pressable>
+                        <Text>Rate us</Text>
+                    </Pressable>
+                </View>
+                {/* divider */}
+                <View>
+                    <Pressable>
+                        <Text>We're on scial media</Text>
+                    </Pressable>
+                    <Pressable>
+                        <Text>TV Stickers</Text>
+                    </Pressable>
+                    <Pressable>
+                        <Text>About</Text>
+                    </Pressable>
+                </View>
+                {/* divider */}
+                <View>
+                    <Pressable>
+                        <Text>Sign out</Text>
+                    </Pressable>
+                </View>
+                {/* divisder */}
+                <View>
+                    <Pressable>
+                        <Text>Sign out</Text>
+                    </Pressable>
+                </View>
+            </ScrollView>
         </View>
     );
 }
