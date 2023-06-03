f0bed9d37dd72aa022d5823345d7417685945a35
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-03 13:10:15 +0200

    asd

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+// import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
@@ -6,25 +6,26 @@ import React from "react";
 
 export default function ChartScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Menu</Text>
+        // <View style={styles.container}>
+        <View>
+            <Text className="text-9xl text-green-500">Menu</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         alignItems: "center",
+//         justifyContent: "center",
+//     },
+//     title: {
+//         fontSize: 20,
+//         fontWeight: "bold",
+//     },
+//     separator: {
+//         marginVertical: 30,
+//         height: 1,
+//         width: "80%",
+//     },
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./components/*.{js,jsx,ts,tsx}", "./app/(tabs)/*.{js,jsx,ts,tsx}"],
+    content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/(tabs)/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
